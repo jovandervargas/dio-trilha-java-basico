3e62791058cfbaa36ad877ff1a3d0fb84649f39a
jovandervargas <jovandervargas2.0@gmail.com>
2024-07-08 21:22:05 -0300

    aprendendo

--- tipo-variaveis/src/tiposvariaveis.java
+++ tipo-variaveis/src/tiposvariaveis.java
@@ -1,5 +1,16 @@
 public class tiposvariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 2500.33;
+       
+        int numeroCurto = 3;
+           numeroCurto = 2;
+        
+          if (numeroCurto==1)
+        System.out.println("você esta aprendendo");
+        else
+        System.out.println("você não esta aprendendo");
+
+
+        
     }
 }
